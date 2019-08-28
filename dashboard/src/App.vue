5032ae4dab2eeb8bd35f994da28b97086f66f8cb
ariverak <jrivera.v@outlook.com>
2019-08-28 19:31:22 -0400

    up dashboard example

---
+++ dashboard/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <dashboard v-if="isLogged" @loginOut="loginOut" />
+    <login @loginIn="loginIn" v-else />
+  </div>
+</template>
+
+<script>
+import Dashboard from './pages/dashboard'
+import Login from './pages/login'
+let isLogged = false;
+export default {
+  name: 'app',
+  components: {
+    Dashboard,
+    Login
+  },
+  methods: {
+    loginIn(){
+      this.isLogged = true;
+    },
+    loginOut(){
+      this.isLogged = false;
+    }
+  },
+  data() {
+    return {
+      isLogged : isLogged
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ dashboard/src/pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app>
+        <div>
+            <v-btn @click="onLogin" class="primary">Entrar</v-btn>
+        </div>
+    </v-app>
+</template>
+<script>
+export default {
+    methods: {
+        onLogin(){
+            this.$emit("loginIn")
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
